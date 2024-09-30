f14f8972b99073da197a263dbd60e3206f2a8755
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-09-30 22:02:18 +0200

    rockchip: add adakta-ad-rk3588 and adakta-mod-rk3588

---
+++ include/configs/adakta-ad-rk3588.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier:     GPL-2.0+
+ *
+ * Copyright (c) 2024 Adakta Ltd.
+ */
+
+#ifndef __CONFIGS_ADAKTA_AD_RK3588_H
+#define __CONFIGS_ADAKTA_AD_RK3588_H
+
+#include <configs/adakta-rk3588.h>
+
+#endif
