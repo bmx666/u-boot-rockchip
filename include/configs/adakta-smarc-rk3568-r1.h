c019547bbb3f454d242fe0f227de3c1a9d85557c
Maxim Paymushkin <bmx666@users.noreply.github.com>
2025-01-15 20:20:32 +0100

    include: configs: rk3568-adakta-smarc: move to r1

--- include/configs/adakta-smarc-rk3568-r1.h
+++ include/configs/adakta-smarc-rk3568-r1.h
@@ -4,8 +4,8 @@
  * Copyright (c) 2024 Adakta Ltd.
  */
 
-#ifndef __CONFIGS_ADAKTA_SMARC_RK3568_H
-#define __CONFIGS_ADAKTA_SMARC_RK3568_H
+#ifndef __CONFIGS_ADAKTA_SMARC_RK3568_REV1_H
+#define __CONFIGS_ADAKTA_SMARC_RK3568_REV1_H
 
 #include <configs/adakta-rk3568.h>
 
